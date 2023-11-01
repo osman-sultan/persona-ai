f6863753e3cabb28318182ff08e2a98ebd1c7f63
Osman Sultan <osmansultan2002@gmail.com>
2023-11-01 01:13:46 -0400

    fix type error

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -2,14 +2,18 @@ import { Sidebar } from "@/components/sidebar";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Menu } from "lucide-react";
 
-export const MobileSidebar = () => {
+interface MobileNavbarProps {
+  isPro: boolean;
+}
+
+export const MobileSidebar = ({ isPro }: MobileNavbarProps) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
