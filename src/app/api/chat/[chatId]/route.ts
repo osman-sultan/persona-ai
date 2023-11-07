de566ef8fd542d70fe9b7cedec1c1bb79978100a
Osman Sultan <osmansultan2002@gmail.com>
2023-11-07 08:32:58 -0500

    use 13b-chat instead

--- src/app/api/chat/[chatId]/route.ts
+++ src/app/api/chat/[chatId]/route.ts
@@ -113,14 +113,12 @@ export async function POST(
     console.log("chainPrompt: ", chainPrompt.template);
 
     const response = await replicate.predictions.create({
-      // You must enable streaming.
       stream: true,
-      // The model must support streaming. See https://replicate.com/docs/streaming
-      // This is the model ID for Llama 2 70b Chat
       version:
-        "2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
-      // Format the message list into the format expected by Llama 2
-      // @see https://github.com/vercel/ai/blob/99cf16edf0a09405d15d3867f997c96a8da869c6/packages/core/prompts/huggingface.ts#L53C1-L78C2
+        // 70b-chat
+        //"2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
+        //13b-chat
+        "f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d",
       input: {
         prompt: chainPrompt.template,
       },
